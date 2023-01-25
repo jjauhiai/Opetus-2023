efb6884e2e47e7071ce6a9f5807ce88bd809da4d
jjauhiai <jukka.jauhiainen@oamk.fi>
2023-01-25 12:03:53 +0200

    Rajapinnat-harjoitus2, kevät 2023. Book ja borrower

---
+++ Harjoitus2/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+var bookRouter = require('./routes/book');
+var borrowerRouter = require('./routes/borrower');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors());
+
+
+app.use('/book', bookRouter);
+app.use('/borrower', borrowerRouter);
+
+module.exports = app;
