a4b524b6bf8f7048b3de04139d814d8f582422d3
jjauhiai <jukka.jauhiainen@oamk.fi>
2023-02-02 18:18:34 +0200

    myAuthorizer, hakee kt+pwd taulusta

--- Harjoitus3/app.js
+++ Harjoitus3/app.js
@@ -4,8 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const db = require('./database');
+const bcrypt = require('bcryptjs');
 const basicAuth = require('express-basic-auth');
-
 
 
 var bookRouter = require('./routes/book');
@@ -21,9 +22,42 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 app.use(cors());
-app.use(basicAuth({users: { 'admin': '1234' }}))
+
+//app.use(basicAuth({users: { 'admin': '1234' }}))
+app.use(basicAuth( { authorizer: myAuthorizer, authorizeAsync:true, } ))
+
 app.use('/book', bookRouter);
 app.use('/borrower', borrowerRouter);
 app.use('/user', userRouter);
 
+function myAuthorizer(username, password,cb){
+    db.query('SELECT password FROM user WHERE username = ?',[username], 
+      function(dbError, dbResults, fields) {
+        if(dbError){
+              response.json(dbError);
+            }
+        else {
+          if (dbResults.length > 0) {
+            bcrypt.compare(password,dbResults[0].password, 
+              function(err,res) {
+                if(res) {
+                  console.log("succes");
+                  return cb(null, true);
+                }
+                else {
+                  console.log("wrong password");
+                  return cb(null, false);
+                }			
+                response.end();
+              }
+            );
+          }
+          else{
+            console.log("user does not exists");
+            return cb(null, false);
+          }
+        }
+      }
+    );
+  }
 module.exports = app;
